445c739f1caae4e19005369c7fba591aae3e2950
xansiety <ferando543@outlook.com>
2022-10-23 10:25:06 -0500

    Acceso con Google y logout implementado

---
+++ src/components/NavBar.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { ref } from "vue";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+import EssentialLink from "components/EssentialLink.vue";
+import { auth } from "src/firebase/firebaseConfig";
+import { async } from "@firebase/util";
+const essentialLinks = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+];
+const rightDrawerOpen = ref(false);
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+
+const accessGoogle = async () => {
+  console.log("Iniciando sesion");
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+
+    console.log(token, user);
+  } catch (error) {
+    console.error(error);
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  }
+};
+
+const logoutGoogle = async () => {
+  console.log("Cerrando sesion");
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<template>
+  <q-header bordered class="bg-primary text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+        Qusar chat
+      </q-toolbar-title>
+
+      <q-btn
+        color="secondary"
+        icon="mdi-google"
+        label="Ingresar"
+        @click="accessGoogle"
+      />
+      <q-btn
+        color="accent"
+        icon="mdi-logout-variant"
+        label="Salir"
+        @click="logoutGoogle"
+      />
+      <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+    </q-toolbar>
+  </q-header>
+
+  <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+    <q-list>
+      <q-item-label header> Essential Links </q-item-label>
+
+      <EssentialLink
+        v-for="link in essentialLinks"
+        :key="link.title"
+        v-bind="link"
+      />
+    </q-list>
+  </q-drawer>
+</template>
+<style lang="scss" scoped></style>
