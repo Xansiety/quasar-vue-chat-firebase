1d797174c90627f6669c3fcb6732208b96dd3fe4
xansiety <ferando543@outlook.com>
2022-10-23 14:03:57 -0500

    Se habilita pligin Notify

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,21 +2,31 @@
 import { ref } from "vue";
 import { collection, addDoc } from "firebase/firestore";
 import { auth, db } from "src/firebase/firebaseConfig";
+import { useQuasar } from "quasar";
 
+const $q = useQuasar();
 const message = ref("");
 
 const sendMessage = () => {
-  addDoc(collection(db, "chat"), {
-    message: message.value,
-    uid: auth.currentUser.uid, //no trae el usuario activo y viene por defecto
-    time: Date.now(),
-    displayName: auth.currentUser.displayName,
-  })
-    .then(() => {
-      console.log("Reproducir sonido");
-      message.value = "";
+  if (message.value === "") {
+    $q.notify({
+      type: "negative",
+      message: "No puedes enviar mensajes vacios",
+      position: "top",
+    });
+  } else {
+    addDoc(collection(db, "chat"), {
+      message: message.value,
+      uid: auth.currentUser.uid, //no trae el usuario activo y viene por defecto
+      time: Date.now(),
+      displayName: auth.currentUser.displayName,
     })
-    .catch((error) => console.error(error));
+      .then(() => {
+        console.log("Reproducir sonido");
+        message.value = "";
+      })
+      .catch((error) => console.error(error));
+  }
 };
 </script>
 
@@ -27,7 +37,7 @@ const sendMessage = () => {
         dark
         dense
         standout
-        v-model="message"
+        v-model.trim="message"
         class="q-ml-md full-width"
         @keyup.enter="sendMessage"
       >
