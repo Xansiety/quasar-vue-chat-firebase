20346d31ba077bc393adf29aad15ce135276127e
xansiety <ferando543@outlook.com>
2022-10-23 09:49:27 -0500

    Creacion proyecto +  Archivo configuracion Firebase

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          name="me"
+          :text="['hey, how are you?']"
+          stamp="7 minutes ago"
+          sent
+          bg-color="amber-7"
+        />
+        <q-chat-message
+          name="Xansiety"
+          :text="['Fine and u?']"
+          stamp="5 minutes ago"
+          bg-color="amber-7"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
